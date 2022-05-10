f806b22f499844a425fe80eeafc373fa96bca631
Ivaelo <62559591+Ivaelo@users.noreply.github.com>
2022-05-10 10:28:15 +0300

    Update settings.py

--- blackjack/blackjack/settings.py
+++ blackjack/blackjack/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',  
         'NAME': 'Blackjack',  
         'USER': 'root',  
-        'PASSWORD': 'rootkotka',  
+        'PASSWORD': '123456789a',  
         'HOST': '127.0.0.1',  
         'PORT': '3306',  
 	}
